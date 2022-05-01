21bd1e7e1b766821afc63cd07a18e6ab9deefef2
jack.nong <771440224@qq.com>
2022-05-01 16:58:08 +0800

    rafactor: 把 amountFor方法 搬移到 PerformanceCalculator 类中

--- restructure/001-statement/createStatementData.js
+++ restructure/001-statement/createStatementData.js
@@ -1,7 +1,7 @@
 /*
  * @Author: vayne
  * @Date: 2022-04-29 17:01:55
- * @LastEditTime: 2022-05-01 16:40:07
+ * @LastEditTime: 2022-05-01 16:57:14
  * @LastEditors: vayne.nong
  * @Description: 创建 StatementData
  */
@@ -26,34 +26,14 @@ function createStatementData(invoice, plays) {
     const calculator = new PerformanceCalculator(aPerformances, playFor(aPerformances));
     const result = Object.assign({}, aPerformances);
     result.play = calculator.play;
-    result.amount = amountFor(result);
+    // result.amount = amountFor(result);
+    result.amount = calculator.amount
     result.volumeCredits = volumeCreditsFor(result);
 
     return result;
 
     function playFor(aPerformances) {
       return plays[aPerformances.playID];
-    }
-
-    function amountFor(aPerformances) {
-      let result = 0;
-      switch (aPerformances.play.type) {
-        case 'tragedy':
-          result = 40000;
-          if (aPerformances.audience > 30) {
-            result += 1000 * (aPerformances.audience - 30);
-          }
-          break;
-        case 'comedy':
-          result = 30000;
-          if (aPerformances.audience > 20) {
-            result += 10000 + 500 * (aPerformances.audience - 20);
-          }
-          break;
-        default:
-          throw new Error(`unknown type: ${aPerformances.play.type}`);
-      }
-      return result;
     }
 
     function volumeCreditsFor(aPerformances) {
@@ -70,7 +50,26 @@ class PerformanceCalculator {
   constructor(aPerformances, aPlay) {
     this.performances = aPerformances;
     this.play = aPlay;
-
+  }
+  get amount() {
+    let result = 0;
+      switch (this.play.type) {
+        case 'tragedy':
+          result = 40000;
+          if (this.performances.audience > 30) {
+            result += 1000 * (this.performances.audience - 30);
+          }
+          break;
+        case 'comedy':
+          result = 30000;
+          if (this.performances.audience > 20) {
+            result += 10000 + 500 * (this.performances.audience - 20);
+          }
+          break;
+        default:
+          throw new Error(`unknown type: ${this.play.type}`);
+      }
+      return result;
   }
 }
 
